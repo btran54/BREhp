32a178ec0bc8efc6c89e79ef37addd1c87cc5d96
btran54 <btran54@ucsc.edu>
2025-10-05 21:55:50 -0700

    Updated API URL for production

--- server/server.js
+++ server/server.js
@@ -7,14 +7,19 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:8000',
+    'http://0.0.0.0:8000',
+    'https://btran54.github.io'  
+  ]
+}));
+
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('MongoDB connection error:', err));
 
